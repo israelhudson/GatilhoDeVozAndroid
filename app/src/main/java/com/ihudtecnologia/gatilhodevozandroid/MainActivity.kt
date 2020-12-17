c9d1c30caf5f86c445af087d9b72ff8b1b48538c
Israel Hudson <israelhudson@gmail.com>
2020-12-16 22:50:59 -0300

    reconhecendo comando de gatilho com porcentagem

--- app/src/main/java/com/ihudtecnologia/gatilhodevozandroid/MainActivity.kt
+++ app/src/main/java/com/ihudtecnologia/gatilhodevozandroid/MainActivity.kt
@@ -2,10 +2,35 @@ package com.ihudtecnologia.gatilhodevozandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var soundClassifier: SoundClassifier
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.gatilho) as TextView
+
+        soundClassifier = SoundClassifier(this, SoundClassifier.Options()).also {
+            it.lifecycleOwner = this
+        }
+
+        //textView.text = "teste"
+        textView.text = soundClassifier.labelList[1] + " - " + soundClassifier.labelList.indexOf("Ola Ana")
+
+        soundClassifier.probabilities.observe(this) { resultMap ->
+            val label = resultMap["Ola Ana"]
+            val probability = resultMap["Ola Ana"] ?: 0f
+            var percent = (probability * 100).toInt()
+            Log.w("ISRAEL", "${(probability * 100).toInt()}")
+
+            textView.text =
+                soundClassifier.labelList[1] + " - " + percent
+
+        }
     }
 }
